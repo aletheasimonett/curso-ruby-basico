8888717d66fd9a194ba9294de48024bf464444a4
Alethea Soares <alethea.tec@gmail.com>
2019-12-05 18:50:08 +0000

    Adicionado exemplo e desafio da aula2

--- aula2/challenge.rb
+++ aula2/challenge.rb
@@ -20,19 +20,19 @@ loop do
 
     if option == 4
         mult = num1 * num2
-        puts("O resultado da multiplicacao eh: #{mult}")
+        puts("O resultado da multiplicacao eh: #{mult} ")
 
     elsif option == 3
         div = num1/num2
-        puts("O resultado da divisão eh: #{div}")
+        puts("O resultado da divisão eh: #{div} ")
 
     elsif option == 2
         subt = num1-num2
-        puts("O resultado da subtracao eh: #{subt}")
+        puts("O resultado da subtracao eh: #{subt} ")
 
     elsif option == 1
         soma = num1 + num2
-        puts("O resultado da soma eh: #{soma}")
+        puts("O resultado da soma eh: #{soma} ")
 
     end
 end
